40e0f56acda33192708d63d0ff42ad3166653cbb
John Nguyen <jnguyen92780@gmail.com>
2020-12-19 15:51:04 -0800

    mapping out the jobs in the Job component

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import { Container } from 'react-bootstrap';
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
-  const { jobs, loading, error} = useFetchJobs(params, page);
+  const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
-      {<h1>{jobs.length}</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   )
 }
