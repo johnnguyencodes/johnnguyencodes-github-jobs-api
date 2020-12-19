a6b559620e09579091c3090f7ddb023c69f0f0ff
John Nguyen <jnguyen92780@gmail.com>
2020-12-19 14:52:09 -0800

    defined ACTION variable for reducer

--- src/useFetchJobs.jsx
+++ src/useFetchJobs.jsx
@@ -1,7 +1,18 @@
 import { useReducer } from 'react';
 
+const ACTIONS = {
+  MAKE_REQUEST: 'make-request',
+  GET_DATA: 'get-data',
+  ERROR: 'error'
+}
+
+function reducer(state, action) {
+
+}
+
 export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true, error: false})
+
   return {
     jobs: [],
     loading: false,
