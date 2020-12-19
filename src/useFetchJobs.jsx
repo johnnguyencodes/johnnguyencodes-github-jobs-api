5631a3baef4c6e3dc1e508b5d9f732cd0260656c
John Nguyen <jnguyen92780@gmail.com>
2020-12-19 14:42:56 -0800

    assigning useReducer to constant variables, useReducer is taking in the initial default states

--- src/useFetchJobs.jsx
+++ src/useFetchJobs.jsx
@@ -1,6 +1,7 @@
 import { useReducer } from 'react';
 
 export default function useFetchJobs(params, page) {
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true, error: false})
   return {
     jobs: [],
     loading: false,
