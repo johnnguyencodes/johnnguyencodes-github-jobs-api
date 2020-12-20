9ba65f8f96c420e9595067020f165cb3639a9fd9
John Nguyen <jnguyen92780@gmail.com>
2020-12-20 15:53:05 -0800

    declared onChange and onError functions

--- src/usePosition.jsx
+++ src/usePosition.jsx
@@ -4,6 +4,13 @@ export default function usePosition() {
   const [position, setPosition] = useState({});
   const [error, setError] = useState(null);
 
+  const onChange = ({coords}) => {
+    setPosition({
+      latitude: coords.latitude,
+      longitude: coords.longitude
+    });
+  }
+
   useEffect(() => {
     const geo = navigator.geolocation;
     if (!geo) {
