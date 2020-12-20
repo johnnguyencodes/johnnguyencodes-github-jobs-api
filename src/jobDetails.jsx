908a0a8f3d973dd063dda5712f573e9ca87f97b4
John Nguyen <jnguyen92780@gmail.com>
2020-12-20 10:25:31 -0800

    added back-link to go back to results

--- src/jobDetails.jsx
+++ src/jobDetails.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export default function JobDetails = ({ details, onResetPage }) => {
-  return ()
+  return (
+    <div className="job-details">
+      <div className="back-link">
+        <a href="/#" onClick={onResetPage}>
+          $lt;$lt; Back to results
+          </a>
+        </div>
+    </div>
+  )
 
 }
