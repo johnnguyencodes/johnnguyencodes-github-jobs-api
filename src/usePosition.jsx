60d5db6e7a3c36180570995131ef2656a076ab7a
John Nguyen <jnguyen92780@gmail.com>
2020-12-20 15:36:27 -0800

    declared const variables for position and error

--- src/usePosition.jsx
+++ src/usePosition.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react';
 
 export default function usePosition() {
-
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
 }
