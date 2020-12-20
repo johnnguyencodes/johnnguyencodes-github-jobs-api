2f3643031c38278cdb8535b1c3c8323ec1fd7453
John Nguyen <jnguyen92780@gmail.com>
2020-12-20 15:38:20 -0800

    declared a useEffect hook which checks if the browser supports geolocation

--- src/usePosition.jsx
+++ src/usePosition.jsx
@@ -3,4 +3,14 @@ import {useState, useEffect} from 'react';
 export default function usePosition() {
   const [position, setPosition] = useState({});
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const geo = navigator.geolocation;
+    if (!geo) {
+      setError ('Geolocation is not supported');
+      return;
+    }
+  })
+
+  return {...position, error};
 }
