c563d5ee7e8f5316c8302b01ca01f37b91d6f726
John Nguyen <jnguyen92780@gmail.com>
2020-12-19 18:24:06 -0800

    added styling to scrollbar and job-card height

--- src/job.jsx
+++ src/job.jsx
@@ -5,22 +5,26 @@ import moment from 'moment';
 
 export default function Job({ job }) {
   return (
-    <Card className="job-card d-flex justify-content-center m-3">
-      <Card.Header>
+    <Card className="job-card d-flex justify-content-center m-3 h-100">
+      <Card.Header className="job-logo">
         <img className="d-none d-md-block" height="50" width="50" src={job.company_logo} alt={job.company} />
       </Card.Header>
-      <Card.Body>
-          <Card.Subtitle className="text-muted mb-2">
+      <Card.Body className="job-body m-0">
+          <Card.Subtitle className="job-date text-muted mb-2">
             {moment(new Date(job.created_at)).fromNow()} &#8226; {job.type}
           </Card.Subtitle>
           <div>
-            <Card.Title>
-              {job.title} - <span className="text-muted font-weight-light">
+            <Card.Title className="job-title">
+              {job.title}
+            </Card.Title>
+            <Card.Subtitle className="job-company my-3">
+              <span className="text-muted font-weight-light">
                 {job.company}
               </span>
-            </Card.Title>
-            {/* <Badge variant="secondary" className="mr-2">{job.type}</Badge> */}
-            <Badge variant="secondary text-break">{job.location}</Badge>
+            </Card.Subtitle>
+            <Card.Subtitle className="job-location font-weight-bold pt-3">
+              {job.location}
+            </Card.Subtitle>
           </div>
       </Card.Body>
     </Card>
