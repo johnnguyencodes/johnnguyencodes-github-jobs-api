4d294b13f9a8e3284d780cb8d7641e9279b227d0
John Nguyen <jnguyen92780@gmail.com>
2020-12-19 18:08:34 -0800

    added styling to job-card for margins

--- src/job.jsx
+++ src/job.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import { Card, Badge } from 'react-bootstrap';
 import moment from 'moment';
-import ReactMarkdown from 'react-markdown';
+// import ReactMarkdown from 'react-markdown';
 
 export default function Job({ job }) {
   return (
-    <Card className="job-card col-3 offset-1 m-2">
+    <Card className="job-card d-flex justify-content-center m-3">
+      <Card.Header>
+        <img className="d-none d-md-block" height="50" width="50" src={job.company_logo} alt={job.company} />
+      </Card.Header>
       <Card.Body>
-        <div className="d-flex justify-content-between">
+          <Card.Subtitle className="text-muted mb-2">
+            {moment(new Date(job.created_at)).fromNow()} &#8226; {job.type}
+          </Card.Subtitle>
           <div>
             <Card.Title>
               {job.title} - <span className="text-muted font-weight-light">
                 {job.company}
               </span>
             </Card.Title>
-            <Card.Subtitle className="text-muted mb-2">
-              Posted {moment(new Date(job.created_at)).fromNow()}
-            </Card.Subtitle>
-            <Badge variant="secondary" className="mr-2">{job.type}</Badge>
-            <Badge variant="secondary" style={{ wordBreak: 'break-all' }}>{job.location}</Badge>
+            {/* <Badge variant="secondary" className="mr-2">{job.type}</Badge> */}
+            <Badge variant="secondary text-break">{job.location}</Badge>
           </div>
-          <img className="d-none d-md-block" height="50" width="50" src={job.company_logo} alt={job.company} />
-        </div>
       </Card.Body>
     </Card>
   )
