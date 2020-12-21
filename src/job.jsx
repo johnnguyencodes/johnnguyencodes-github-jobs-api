36bae2b87ef2bbca1cbf2e5ebf4b1fd073b062b1
John Nguyen <jnguyen92780@gmail.com>
2020-12-20 23:02:15 -0800

    touched up job cards styling

--- src/job.jsx
+++ src/job.jsx
@@ -6,12 +6,10 @@ export default function Job({ job, onItemClick }) {
   const string = job.company;
   const companyInitial = string.substring(0, 1);
   return (
-    <Card className="job-card d-flex justify-content-center m-3 h-100 border-0" onClick={() => onItemClick(job.id)}>
-      <Card.Header className="job-card-header border-0">
-        {job.company_logo ? <img className="d-none d-md-block company-logo" height="50" width="50" src={job.company_logo} alt={job.company} />
-                          : <h1><span class="badge font-weight-bold text-white company-logo d-flex justify-content-center align-items-center">{companyInitial}</span></h1> }
-      </Card.Header>
+    <Card className="job-card d-flex justify-content-center mx-3 my-4 h-100 border-0" onClick={() => onItemClick(job.id)}>
       <Card.Body className="job-card-body pt-5 mt-2">
+                {job.company_logo ? <img className="d-none d-md-block company-logo" height="50" width="50" src={job.company_logo} alt={job.company} />
+                          : <h1><span class="badge font-weight-bold text-white company-logo d-flex justify-content-center align-items-center">{companyInitial}</span></h1> }
           <Card.Subtitle className="job-card-date text-muted font-weight-bold mb-2">
             {moment(new Date(job.created_at)).fromNow()} &#8226; {job.type}
           </Card.Subtitle>
