c45d5192f28977da6880b619facea70860641d2a
John Nguyen <jnguyen92780@gmail.com>
2020-12-21 01:03:02 -0800

    styled job-details-header

--- src/jobDetails.jsx
+++ src/jobDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Button } from 'react-bootstrap';
 
 export default function JobDetails({ details, onResetView }) {
 
@@ -14,13 +15,44 @@ export default function JobDetails({ details, onResetView }) {
     how_to_apply
   } = details;
 
+
+  let string = company;
+  let companyInitial;
+  if (string) {
+    companyInitial = string.substring(0, 1);
+  }
+
   return (
-    <div className="job-details-container">
+    <div className="job-details-container col-10 offset-1">
       <div className="back-link">
         <a href="/#" onClick={onResetView}>
           &lt;&lt; Back to results
           </a>
         </div>
+        <header className="details-header m-0 p-0 bg-white row">
+          {company_logo
+            ? <img className="d-none d-md-block details-company-logo"
+              height="150" width="150" src={company_logo} alt={company} />
+            : <h1 className="m-0 p-0"><span
+                    className="badge details-badge font-weight-bold
+                      text-white details-company-logo d-flex justify-content-center
+                      align-items-center m-0 p-0">
+                        {companyInitial ? companyInitial : ''}
+              </span></h1>}
+          <div className="details-header-body col-10 offset-1 d-flex
+            justify-content-between align-items-between p-0 m-0">
+            <div className="d-flex flex-column justify-content-center ml-5">
+              <h1 className="font-weight-bold mb-3">{company}</h1>
+              <p className="text-muted mt-3">{company_url}</p>
+            </div>
+            <div className="d-flex justify-content-center align-items-center">
+              <Button href={company_url} className="button-company-url purple font-weight-bold
+                btn-outline-none">
+                Company Site
+              </Button>
+            </div>
+          </div>
+        </header>
         <div>
           {type} / {location}
         </div>
--- src/searchForm.jsx
+++ src/searchForm.jsx
@@ -9,7 +9,7 @@ export default function SearchForm({ params, onParamChange }) {
           <Form.Group as={Col} className="m-0 p-2">
             <div className="d-flex flex-column">
               <div>
-                <i class="fas fa-search purple fa-2x"></i>
+                <i className="fas fa-search purple fa-2x"></i>
               </div>
               <Form.Control className="border-top-0 border-bottom-0 border-left-0 border-right pl-5 input" onChange={onParamChange} value={params.description} placeholder="Filter by title, companies, expertise..." name="description" type="text" />
             </div>
@@ -17,7 +17,7 @@ export default function SearchForm({ params, onParamChange }) {
           <Form.Group as={Col} className="m-0 p-2">
             <div className="d-flex flex-column">
               <div>
-                <i class="fas fa-map-marker-alt purple fa-2x"></i>
+                <i className="fas fa-map-marker-alt purple fa-2x"></i>
               </div>
             <Form.Control className="border-top-0 border-bottom-0 border-left-0 border-right pl-5 input" onChange={onParamChange} value={params.location} placeholder="Filter by location..." name="location" type="text" />
           </div>
